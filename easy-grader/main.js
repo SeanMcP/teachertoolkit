348e5c32bb044d1ee41f341a68223dbb0d12ec0a
Sean McPherson <seanmcp25@gmail.com>
2019-03-15 06:59:47 -0400

    Style grade table

--- easy-grader/main.js
+++ easy-grader/main.js
@@ -12,26 +12,42 @@ function generateTable() {
         isError = true;
     }
     if (places.length === 0) {
-        displayErrorOn('placesLabel')
+        displayErrorOn('placesLabel');
         isError = true;
     }
     if (isError) {
         return;
     }
     clearTable();
+    var fractionSpanWidth;
+    var percentSpanWidth;
     for (var i = 0; i <= problems; i++) {
         var value = problems - i;
         var li = document.createElement('li');
+        li.classList.add('GradeList__item');
 
         var fractionSpan = document.createElement('span');
+        fractionSpan.classList.add('GradeList__value', 'GradeList__fraction');
         fractionSpan.textContent = value + '/' + problems;
+        if (fractionSpanWidth) {
+            fractionSpan.style.width = fractionSpanWidth + 'px';
+        }
         li.appendChild(fractionSpan);
-
+        
         var percentSpan = document.createElement('span');
-        percentSpan.textContent = (100 * value / problems).toFixed(places) + '%';
+        percentSpan.classList.add('GradeList__value', 'GradeList__percent');
+        percentSpan.textContent = ((100 * value) / problems).toFixed(places) + '%';
+        if (percentSpanWidth) {
+            percentSpan.style.width = percentSpanWidth + 'px';
+        }
         li.appendChild(percentSpan);
-
+        
         outputList.appendChild(li);
+
+        if (i === 0) {
+            fractionSpanWidth = fractionSpan.getBoundingClientRect().width;
+            percentSpanWidth = percentSpan.getBoundingClientRect().width;
+        }
     }
 }
 
@@ -42,14 +58,16 @@ function clearTable() {
 }
 
 function clearErrors() {
-    document.querySelectorAll('.ErrorMessage').forEach(node => node.remove());
-    document.querySelectorAll('.--has-error').forEach(node => node.classList.remove('--has-error'));
+    document.querySelectorAll('.ErrorMessage').forEach((node) => node.remove());
+    document
+        .querySelectorAll('.--has-error')
+        .forEach((node) => node.classList.remove('--has-error'));
 }
 
 function displayErrorOn(id) {
     var el = document.getElementById(id);
     el.classList.add('--has-error');
-    var p = document.createElement('p')
+    var p = document.createElement('p');
     p.classList.add('ErrorMessage');
     p.textContent = 'Uh oh! This value must be a valid number.';
     el.appendChild(p);
