7e4f3badd3ed1af12eed26e9add9203057fc78d5
Sean McPherson <seanmcp25@gmail.com>
2019-03-21 06:21:34 -0400

    Add fetch listener to service worker

--- register.js
+++ register.js
@@ -2,14 +2,12 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         navigator.serviceWorker.register('serviceWorker.js').then(
             function(registration) {
-                // Registration was successful
                 console.log(
                     'ServiceWorker registration successful with scope: ',
                     registration.scope
                 );
             },
             function(err) {
-                // registration failed :(
                 console.log('ServiceWorker registration failed: ', err);
             }
         );
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,11 @@
+/*
+    I'm still working to understand all of this. The code below
+    was copied and lightly modified from the Google Web Dev
+    guide on service workers.
+
+    URL: https://developers.google.com/web/fundamentals/primers/service-workers/
+*/
+
 var CACHE_NAME = 'teacher-toolkit-v0';
 var urlsToCache = [
     '/',
@@ -11,8 +19,42 @@ self.addEventListener('install', function(event) {
     // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME).then(function(cache) {
-            console.log('Opened cache');
+            // console.log('Opened cache');
             return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request).then(function(response) {
+                // Check if we received a valid response
+                if (
+                    !response ||
+                    response.status !== 200 ||
+                    response.type !== 'basic'
+                ) {
+                    return response;
+                }
+
+                // IMPORTANT: Clone the response. A response is a stream
+                // and because we want the browser to consume the response
+                // as well as the cache consuming the response, we need
+                // to clone it so we have two streams.
+                var responseToCache = response.clone();
+
+                caches.open(CACHE_NAME).then(function(cache) {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return response;
+            });
         })
     );
 });
