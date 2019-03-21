46550c101501eeb273542c9e1eb1ad25f974c6be
Sean McPherson <seanmcp25@gmail.com>
2019-03-21 06:42:37 -0400

    Update paths
    Register service worker from easy-grader.

--- register.js
+++ register.js
@@ -1,15 +1,17 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('serviceWorker.js').then(
-            function(registration) {
-                console.log(
-                    'ServiceWorker registration successful with scope: ',
-                    registration.scope
-                );
-            },
-            function(err) {
-                console.log('ServiceWorker registration failed: ', err);
-            }
-        );
+        navigator.serviceWorker
+            .register('/serviceWorker.js')
+            .then(
+                function(registration) {
+                    console.log(
+                        'ServiceWorker registration successful with scope: ',
+                        registration.scope
+                    );
+                },
+                function(err) {
+                    console.log('ServiceWorker registration failed: ', err);
+                }
+            );
     });
 }
