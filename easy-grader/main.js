60662a33c9f9af9cb83871f5c9413e0920fe5ac0
Sean McPherson <seanmcp25@gmail.com>
2019-04-19 10:57:56 -0400

    Add prettier

--- easy-grader/main.js
+++ easy-grader/main.js
@@ -1,81 +1,87 @@
-(function() {
-    var outputList = document.getElementById('outputList');
-    var problemsInput = document.getElementById('problemsInput');
-    var placesInput = document.getElementById('placesInput');
-    
+;(function() {
+    var outputList = document.getElementById('outputList')
+    var problemsInput = document.getElementById('problemsInput')
+    var placesInput = document.getElementById('placesInput')
+
     function generateTable() {
-        clearErrors();
-        var isError = false;
-        var problems = problemsInput.value;
-        var places = placesInput.value;
+        clearErrors()
+        var isError = false
+        var problems = problemsInput.value
+        var places = placesInput.value
         if (problems.length === 0) {
-            displayErrorOn('problemsLabel');
-            isError = true;
+            displayErrorOn('problemsLabel')
+            isError = true
         }
         if (places.length === 0) {
-            displayErrorOn('placesLabel');
-            isError = true;
+            displayErrorOn('placesLabel')
+            isError = true
         }
         if (isError) {
-            return;
+            return
         }
-        clearTable();
-        var fractionSpanWidth;
-        var percentSpanWidth;
+        clearTable()
+        var fractionSpanWidth
+        var percentSpanWidth
         for (var i = 0; i <= problems; i++) {
-            var value = problems - i;
-            var li = document.createElement('li');
-            li.classList.add('GradeList__item');
-    
-            var fractionSpan = document.createElement('span');
-            fractionSpan.classList.add('GradeList__value', 'GradeList__fraction');
-            fractionSpan.textContent = value + '/' + problems;
+            var value = problems - i
+            var li = document.createElement('li')
+            li.classList.add('GradeList__item')
+
+            var fractionSpan = document.createElement('span')
+            fractionSpan.classList.add(
+                'GradeList__value',
+                'GradeList__fraction'
+            )
+            fractionSpan.textContent = value + '/' + problems
             if (fractionSpanWidth) {
-                fractionSpan.style.width = fractionSpanWidth + 'px';
+                fractionSpan.style.width = fractionSpanWidth + 'px'
             }
-            li.appendChild(fractionSpan);
-            
-            var percentSpan = document.createElement('span');
-            percentSpan.classList.add('GradeList__value', 'GradeList__percent');
-            percentSpan.textContent = ((100 * value) / problems).toFixed(places) + '%';
+            li.appendChild(fractionSpan)
+
+            var percentSpan = document.createElement('span')
+            percentSpan.classList.add('GradeList__value', 'GradeList__percent')
+            percentSpan.textContent =
+                ((100 * value) / problems).toFixed(places) + '%'
             if (percentSpanWidth) {
-                percentSpan.style.width = percentSpanWidth + 'px';
+                percentSpan.style.width = percentSpanWidth + 'px'
             }
-            li.appendChild(percentSpan);
-            
-            outputList.appendChild(li);
-    
+            li.appendChild(percentSpan)
+
+            outputList.appendChild(li)
+
             if (i === 0) {
-                fractionSpanWidth = fractionSpan.getBoundingClientRect().width;
-                percentSpanWidth = percentSpan.getBoundingClientRect().width;
+                fractionSpanWidth = fractionSpan.getBoundingClientRect().width
+                percentSpanWidth = percentSpan.getBoundingClientRect().width
             }
         }
     }
-    
+
     function clearTable() {
         while (outputList.firstChild) {
-            outputList.removeChild(outputList.firstChild);
+            outputList.removeChild(outputList.firstChild)
         }
     }
-    
+
     function clearErrors() {
-        document.querySelectorAll('.ErrorMessage').forEach((node) => node.remove());
+        document
+            .querySelectorAll('.ErrorMessage')
+            .forEach(node => node.remove())
         document
             .querySelectorAll('.--has-error')
-            .forEach((node) => node.classList.remove('--has-error'));
+            .forEach(node => node.classList.remove('--has-error'))
     }
-    
+
     function displayErrorOn(id) {
-        var el = document.getElementById(id);
-        el.classList.add('--has-error');
-        var p = document.createElement('p');
-        p.classList.add('ErrorMessage');
-        p.textContent = 'Uh oh! This value must be a valid number.';
-        el.appendChild(p);
+        var el = document.getElementById(id)
+        el.classList.add('--has-error')
+        var p = document.createElement('p')
+        p.classList.add('ErrorMessage')
+        p.textContent = 'Uh oh! This value must be a valid number.'
+        el.appendChild(p)
     }
-    
-    generateTable();
-    
-    problemsInput.addEventListener('change', generateTable);
-    placesInput.addEventListener('change', generateTable);
+
+    generateTable()
+
+    problemsInput.addEventListener('change', generateTable)
+    placesInput.addEventListener('change', generateTable)
 })()
--- register.js
+++ register.js
@@ -1,17 +1,15 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker
-            .register('/serviceWorker.js')
-            .then(
-                function(registration) {
-                    console.log(
-                        'ServiceWorker registration successful with scope: ',
-                        registration.scope
-                    );
-                },
-                function(err) {
-                    console.log('ServiceWorker registration failed: ', err);
-                }
-            );
-    });
+        navigator.serviceWorker.register('/serviceWorker.js').then(
+            function(registration) {
+                console.log(
+                    'ServiceWorker registration successful with scope: ',
+                    registration.scope
+                )
+            },
+            function(err) {
+                console.log('ServiceWorker registration failed: ', err)
+            }
+        )
+    })
 }
